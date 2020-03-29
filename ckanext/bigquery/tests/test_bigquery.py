673e6fae71dd9dc9eacbd74540091938cb345e70
Jovanka Gulicoska <jovanka.gulicoska@datopian.com>
2020-03-29 10:19:35 +0200

    Backend bigquery

---
+++ ckanext/bigquery/tests/test_bigquery.py
@@ -0,0 +1,12 @@
+import os
+from google.cloud import bigquery
+
+# read and setup the credentials for bigquery
+
+def test_search():
+    print 'We are in test search'
+
+if __name__ == "__main__":
+    print 'Starting tests'
+    test_search()
+    print 'End tests'
