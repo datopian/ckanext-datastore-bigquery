d81ef50b7951d0c5eb01cb7ee71d4a03a1e487a9
Rufus Pollock <rufus.pollock@datopian.com>
2020-03-29 22:56:28 +0200

    [ckan2bq/lib][s]: return records in ckan structure.

--- src/ckan_to_bigquery.py
+++ src/ckan_to_bigquery.py
@@ -3,11 +3,30 @@ import os
 from google.cloud import bigquery
 
 def search(table):
+    results = search_raw(table)
+    # TODO: set resource_id, set _links, handle errors, set fields, ...
+    out = {
+        "help": "https://demo.ckan.org/api/3/action/help_show?name=datastore_search",
+        "success": True,
+        "result": {
+            "include_total": True,
+            "resource_id": "553a4d08-7670-48c2-a994-9e180131b22e",
+            "fields": [ ],
+            "records_format": "objects",
+            "records": results,
+            "_links": {
+              "start": "/api/3/action/datastore_search?resource_id=553a4d08-7670-48c2-a994-9e180131b22e",
+              "next": "/api/3/action/datastore_search?offset=100&resource_id=553a4d08-7670-48c2-a994-9e180131b22e"
+             },
+            "total": len(results)
+          }
+      }
+    return out
+
+def search_raw(table):
     client = bigquery.Client()
     projectid = os.environ['BIGQUERY_PROJECT_ID']
     dataset = 'NHS'
-
-    # Perform a query.
     query = 'SELECT * FROM `%s.%s.%s` LIMIT 10' % (projectid, dataset, table)
     query_job = client.query(query)
     rows = query_job.result()
--- test/test_ckan_to_bigquery.py
+++ test/test_ckan_to_bigquery.py
@@ -14,27 +14,39 @@ def fixture_create():
     insert_data(blah)
 
 
-def test_search():
-    # assume there is a bigquery tables created
-    results = ckan_to_bigquery.search(table_name)
+expected1 = {
+        'ACTUAL_COST': 6.59007,
+        'ADDRESS_1': '-', 'ADDRESS_2': '-', 'ADDRESS_3': '-', 'ADDRESS_4': '-',
+        'ADQUSAGE': 0.0, 'AREA_TEAM_CODE': '-', 'AREA_TEAM_NAME': 'UNIDENTIFIED',
+        'BNF_CHAPTER_PLUS_CODE': '20: Dressings',
+        'BNF_CHEMICAL_SUBSTANCE': '2003',
+        'BNF_CODE': '20030100373',
+        'BNF_DESCRIPTION': 'Softpore dressing 10cm x 20cm',
+        'CHEMICAL_SUBSTANCE_BNF_DESCR': 'Wound Management & Other Dressings',
+        'ITEMS': 2, 'NIC': 7.0, 'PCO_CODE': '-', 'PCO_NAME': 'UNIDENTIFIED',
+        'POSTCODE': '-',
+        'PRACTICE_CODE': '-', 'PRACTICE_NAME': 'UNIDENTIFIED DOCTORS',
+        'QUANTITY': 10.0,
+        'REGIONAL_OFFICE_CODE': '-', 'REGIONAL_OFFICE_NAME':
+        'UNIDENTIFIED', 'TOTAL_QUANTITY': 20.0, 'UNIDENTIFIED': True,
+        'YEAR_MONTH': 201401
+    }
+
+def test_search_raw():
+    results = ckan_to_bigquery.search_raw(table_name)
     assert len(results) == 10
     first = results[0]
-    expected = {
-            'ACTUAL_COST': 6.59007,
-            'ADDRESS_1': '-', 'ADDRESS_2': '-', 'ADDRESS_3': '-', 'ADDRESS_4': '-',
-            'ADQUSAGE': 0.0, 'AREA_TEAM_CODE': '-', 'AREA_TEAM_NAME': 'UNIDENTIFIED',
-            'BNF_CHAPTER_PLUS_CODE': '20: Dressings',
-            'BNF_CHEMICAL_SUBSTANCE': '2003',
-            'BNF_CODE': '20030100373',
-            'BNF_DESCRIPTION': 'Softpore dressing 10cm x 20cm',
-            'CHEMICAL_SUBSTANCE_BNF_DESCR': 'Wound Management & Other Dressings',
-            'ITEMS': 2, 'NIC': 7.0, 'PCO_CODE': '-', 'PCO_NAME': 'UNIDENTIFIED',
-            'POSTCODE': '-',
-            'PRACTICE_CODE': '-', 'PRACTICE_NAME': 'UNIDENTIFIED DOCTORS',
-            'QUANTITY': 10.0,
-            'REGIONAL_OFFICE_CODE': '-', 'REGIONAL_OFFICE_NAME':
-            'UNIDENTIFIED', 'TOTAL_QUANTITY': 20.0, 'UNIDENTIFIED': True,
-            'YEAR_MONTH': 201401
-        }
-    assert first == expected
+    assert first == expected1
+
+def test_search():
+    out = ckan_to_bigquery.search(table_name)
+    assert out['result']['total'] == 10
+    first = out['result']['records'][0]
+    assert first == expected1
+
+'''
+def test_search_with_limit():
+    results = ckan_to_bigquery.search(table_name, limit=5)
+    assert len(results) == 5
+'''
 
