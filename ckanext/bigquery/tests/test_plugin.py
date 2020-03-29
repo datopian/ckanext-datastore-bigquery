42b30c05d9f35ed72ca12dc39ebbc05c3eda29a8
Jovanka Gulicoska <jovanka.gulicoska@datopian.com>
2020-03-29 12:51:04 +0200

    test tmp

--- ckanext/bigquery/tests/test_plugin.py
+++ ckanext/bigquery/tests/test_plugin.py
@@ -8,6 +8,9 @@ assert_equals = nose.tools.assert_equals
 assert_raises = nose.tools.assert_raises
 assert_in = nose.tools.assert_in
 
+credentials = config.get('google_cloud_credentials', None)
+client = bigquery.Client.from_service_account_json(credentials)
+
 def test_plugin():
     pass
 
@@ -18,7 +21,19 @@ def test_connect_bigquery():
             - credentials.json file from Google Cloud
     '''
     credentials = config.get('google_cloud_credentials', None)
-    assert_equals(None, credentials, None)
+    if credentials is None:
+        assert_equals(credentials, None)
+
+    # init BiqQuery client
     client = bigquery.Client.from_service_account_json(credentials)
 
-    return client
+
+def test_search_bigquery():
+    QUERY = (
+        'SELECT * FROM `bigquerytest-271707.NHS.201401` '
+        'LIMIT 100')
+
+    query_job = client.query(QUERY)  # API request
+    rows = query_job.result()  # Waits for query to finish
+    for row in rows:
+        print row
